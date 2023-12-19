f7d6a6afa94a604b8c05be8f6190a9ed34b0dcb2
Ollie-Judge <ollie.judge@hotmail.com>
2023-12-19 12:13:30 +0000

    loaded a gif without reloading the page

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-const img = document.querySelector("img");
 const image = document.getElementById("image");
 const reloadButton = document.getElementById("reloadButton");
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=rUNNT3lMDBa7MjCVWPNPQW4Od0v0Tifi&s=cats",
-  {
-    mode: "cors",
-  }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    image.src = response.data.images.original.url;
-  });
-
 reloadButton.addEventListener("click", function () {
-  location.reload();
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=rUNNT3lMDBa7MjCVWPNPQW4Od0v0Tifi&s=cats`,
+    {
+      mode: "cors",
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      image.src = response.data.images.original.url;
+      image.style.visibility = "visible";
+    });
 });
