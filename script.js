aecd6fa7b3fdd3b6e4eeb485138e2fb5c26cf2c4
Ollie-Judge <ollie.judge@hotmail.com>
2023-12-20 11:14:07 +0000

    turned my fetch into an async function

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ const image = document.getElementById("image");
 const gifSearch = document.getElementById("gifSearch");
 const reloadButton = document.getElementById("reloadButton");
 
-reloadButton.addEventListener("click", function () {
-  fetch(
+reloadButton.addEventListener("click", async function () {
+  const response = await fetch(
     `https://api.giphy.com/v1/gifs/translate?api_key=rUNNT3lMDBa7MjCVWPNPQW4Od0v0Tifi&s=${gifSearch.value}`,
     {
       mode: "cors",
     }
-  )
-    .then(function (response) {
-      return response.json();
-    })
+  );
+  response
+    .json()
     .then(function (response) {
       image.src = response.data.images.original.url;
       image.style.visibility = "visible";
